f64c010616ddde37a76303366daea8e989c8be7f
Shourya26-08 <aryanbhatnagar13@gmail.com>
2026-08-23 16:29:06 +0530

    feat: auto-create profile and auth token

---
+++ backend/social/signals.py
@@ -0,0 +1,11 @@
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
+from .models import Profile
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_profile_and_token(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+        Token.objects.create(user=instance)
